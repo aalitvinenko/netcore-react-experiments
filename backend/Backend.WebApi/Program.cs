ac596bfaf03d56f3f98a9c21238d4bf812d8d6ce
Alexander <a.litvinenko@outlook.com>
2021-02-23 18:06:34 +0300

    Move Serilog configuration to appsettings, tweak logging and ElasticSearch configuration

--- backend/Backend.WebApi/Program.cs
+++ backend/Backend.WebApi/Program.cs
@@ -3,12 +3,13 @@ using Backend.Infrastructure.Persistence;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Serilog;
-using Serilog.Sinks.Elasticsearch;
 using System;
+using System.IO;
 using System.Threading.Tasks;
 
 namespace Backend.WebApi
@@ -51,28 +52,23 @@ namespace Backend.WebApi
             await host.RunAsync();
         }
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .UseSerilog(((context, configuration) =>
-                {
-                    configuration.Enrich.FromLogContext()
-                        .Enrich.WithMachineName()
-                        .WriteTo.Console()
-                        .WriteTo.Elasticsearch(
-                            new ElasticsearchSinkOptions(new Uri(context.Configuration["ElasticConfiguration:Uri"]))
-                            {
-                                IndexFormat =
-                                    $"{context.Configuration["ElasticConfiguration:ApplicationName"]}-logs-{context.HostingEnvironment.EnvironmentName?.ToLower().Replace(".", "-")}-{DateTime.UtcNow:yyyy-MM}",
-                                AutoRegisterTemplate = true,
-                                NumberOfShards = 2,
-                                NumberOfReplicas = 1
-                            })
-                        .Enrich.WithProperty("Environment", context.HostingEnvironment.EnvironmentName)
-                        .ReadFrom.Configuration(context.Configuration);
-                }))
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
+        public static IHostBuilder CreateHostBuilder(string[] args)
+        {
+            return Host.CreateDefaultBuilder(args)
+                .ConfigureAppConfiguration(ConfigureAppConfiguration)
+                .UseSerilog((context, configuration) => configuration.ReadFrom.Configuration(context.Configuration))
+                .ConfigureWebHostDefaults(webBuilder => webBuilder.UseStartup<Startup>());
+        }
+
+        private static void ConfigureAppConfiguration(
+            HostBuilderContext builderContext,
+            IConfigurationBuilder configurationBuilder)
+        {
+            var env = builderContext.HostingEnvironment.EnvironmentName;
+            configurationBuilder.SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{env}.json", optional: true, reloadOnChange: true)
+                .AddEnvironmentVariables();
+        }
     }
 }
