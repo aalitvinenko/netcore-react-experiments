dad9d80846cbfa16a435a2263d5faf6fb5a850bf
Alexander <a.litvinenko@outlook.com>
2021-02-27 09:41:06 +0300

    Rename LoginController to AuthController

---
+++ backend/Backend.WebApi/Controllers/AuthController.cs
@@ -0,0 +1,24 @@
+﻿using Backend.Application.ApplicationUser.Queries.GetToken;
+using Backend.Application.Common.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace Backend.WebApi.Controllers
+{
+    /// <summary>
+    /// User authentication and registration controller
+    /// </summary>
+    public class AuthController : BaseApiController
+    {
+        /// <summary>
+        /// Log user in and return a token
+        /// </summary>
+        /// <param name="query">Login query</param>
+        /// <returns>If succeeded, returns user information and token</returns>
+        [HttpPost]
+        public async Task<ActionResult<ServiceResult<LoginResponse>>> Login(GetTokenQuery query)
+        {
+            return Ok(await Mediator.Send(query));
+        }
+    }
+}
